ac4b2342f3deafeb4de06c40db3bdeb160f522ba
jangiethegreat <fernandojohncris@gmail.com>
2023-07-03 15:07:57 +0800

    edit commit

--- resources/views/request_tickets/index.blade.php
+++ resources/views/request_tickets/index.blade.php
@@ -1,42 +1,40 @@
 @extends('layouts.app')
-
+ 
 @section('content')
     <h1>Request Tickets</h1>
-
+ 
     <a href="{{ route('request_tickets.create') }}" class="btn btn-primary mb-3">Create Request Ticket</a>
-
-    @if (session('success'))
-        <div class="alert alert-success">{{ session('success') }}</div>
+   
+        @f (session('success'))
+           <div class="alert alert-success">{{ session('success') }}</div>
     @endif
+ 
+    <div class="row">
+        @forelse ($request_tickets as $request_ticket)
+            <div class="col-md-4 mb-4"
+                <div class="card">
+                    <div class="card-body">
+                        <h5 clas="card-title">Requestor's Name: {{ $request_ticket->requestor_name }}</h5>
+                        <p class="card-text">
+                            <strong>Unit No:</strong> {{ $request_ticket->unit_no }}<br>
+                            <strong>Items Requested:</stron> {{ $request_ticket->items_requested }}<br>
+                            <strong>Quantity:</strong> {{ $request_ticket->quantity }}
+                        </p>
+                        <div class="card-footer d-flex justfy-content-center">
+                            <a href="{{ route('accepted_tickets.create', $request_ticket->id) }}" class="btn btn-success" style="margin:5px;">Accept</a>
 
-    <table class="table">
-    <thead>
-    <tr>
-        <th>Requestor's Name</th>
-        <th>Unit No</th>
-        <th>Items Requested</th>
-        <th>Quantity</th>
-        <th>Action</th>
-    </tr>
-</thead>
-        <tbody>
-            @forelse ($request_tickets as $request_ticket)
-                <tr>
-                    <td>{{ $request_ticket->requestor_name }}</td>
-                    <td>{{ $request_ticket->unit_no }}</td>
-                    <td>{{ $request_ticket->items_requested }}</td>
-                    <td>{{ $request_ticket->quantity }}</td>
-                    <td>
-                    <a href="{{ route('accepted_tickets.create', $request_ticket->id) }}" class="btn btn-success">Accept</a> 
-                    <a href="{{ route('rejected_tickets.create', $request_ticket->id) }}" class="btn btn-danger">Reject</a> 
-                        
-                    </td>
-                </tr>
-            @empty
-                <tr>
-                    <td colspan="4">No request tickets found.</td>
-                </tr>
-            @endforelse
-        </tbody>
-    </table>
-@endsection
+                                               <a href="{{ route('rejected_tickets.create', $request_ticket->id) }}" class="btn btn-danger" style="margin:5px;">Reject</a>
+                                      
+                        </div>
+                                       </div>
+                </div>
+            </d
+iv>
+        @empty
+            <p>No request tickets found.</p>
+         @endforelse
+    
+
+    </div>
+    @endsection
+    
