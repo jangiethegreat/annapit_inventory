149d2d40cd79d0e8659fc1cb48085f681006b2dd
jangiethegreat <fernandojohncris@gmail.com>
2023-07-07 16:19:21 +0800

    card index unfinished commit

--- resources/views/request_tickets/index.blade.php
+++ resources/views/request_tickets/index.blade.php
@@ -12,7 +12,7 @@
     <div class="row">
         @forelse ($request_tickets as $request_ticket)
             <div class="col-md-4 mb-4">
-                <div class="card">
+                <div class="card border-info mb-3">
                     <div class="card-body">
                         <h5 class="card-title">Requestor's Name: {{ $request_ticket->requestor_name }}</h5>
                         <p class="card-text">
@@ -20,7 +20,7 @@
                             <strong>Items Requested:</strong> {{ $request_ticket->items_requested }}<br>
                             <strong>Quantity:</strong> {{ $request_ticket->quantity }}
                         </p>
-                        <div class="card-footer d-flex justify-content-center" style="margin:5px;">
+                        <div class="card-footer d-flex justify-content-center" >
                             <a href="{{ route('accepted_tickets.create', $request_ticket->id) }}" class="btn btn-success">Accept</a>
                             <a href="{{ route('rejected_tickets.create', $request_ticket->id) }}" class="btn btn-danger">Reject</a>
                         </div>
--- resources/views/stocks/index.blade.php
+++ resources/views/stocks/index.blade.php
@@ -9,39 +9,32 @@
         <div class="alert alert-success">{{ session('success') }}</div>
     @endif
 
-    <table class="table">
-        <thead>
-            <tr>
-                <th>Category</th>
-                <th>Quantity</th>
-                <th>Details</th>
-                <th>Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            @forelse ($stocks as $stock)
-                <tr>
-                    <td>{{ $stock->category }}</td> <!-- Display the category name -->
-                    <td>{{ $stock->quantity }}</td>
-                    <td>{{ $stock->details }}</td>
-                    <td>
-                <form action="{{ route('stocks.addtocart', $stock->id) }}" method="POST">
-                   
-                    <button type="submit" class="btn btn-primary">Add to Cart</button>
-
+    <div class="row">
+        @forelse ($stocks as $stock)
+            <div class="col-md-4 mb-4">
+                <div class="card border-secondary mb-3">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $stock->category }}</h5>
+                        <p class="card-text">Quantity: {{ $stock->quantity }}</p>
+                        <p class="card-text">Details: {{ $stock->details }}</p>
+                        <div class="card-footer d-flex justify-content-center" >
+                        <form action="{{ route('stocks.addtocart', $stock->id) }}" method="POST">
+                            <button type="submit" class="btn btn-primary">Add to Cart</button>
+                        </form>
                         <a href="{{ route('stocks.edit', $stock->id) }}" class="btn btn-secondary">Edit</a>
                         <form action="{{ route('stocks.destroy', $stock->id) }}" method="POST" style="display: inline-block;">
                             @csrf
                             @method('DELETE')
                             <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure you want to delete this stock?')">Delete</button>
                         </form>
-                    </td>
-                </tr>
-            @empty
-                <tr>
-                    <td colspan="3">No stocks found.</td>
-                </tr>
-            @endforelse
-        </tbody>
-    </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @empty
+            <div class="col">
+                <p>No stocks found.</p>
+            </div>
+        @endforelse
+    </div>
 @endsection
